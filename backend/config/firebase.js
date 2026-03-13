9ff6c118164ca08a2c917078a98d2817de1db3b7
Yuvasree-M <yuvasreemohan4sep2003@gmail.com>
2026-03-13 16:05:16 +0530

    Ready for Deploy

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,18 +1,6 @@
-import admin from "firebase-admin";
-
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-export const db = admin.firestore();
-export const auth = admin.auth();
-
-// import { readFileSync } from "fs";
 // import admin from "firebase-admin";
 
-// const serviceAccount = JSON.parse(readFileSync("./serviceAccountKey.json", "utf8"));
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
@@ -20,3 +8,15 @@ export const auth = admin.auth();
 
 // export const db = admin.firestore();
 // export const auth = admin.auth();
+
+import { readFileSync } from "fs";
+import admin from "firebase-admin";
+
+// const serviceAccount = JSON.parse(readFileSync("./serviceAccountKey.json", "utf8"));
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export const db = admin.firestore();
+export const auth = admin.auth();
