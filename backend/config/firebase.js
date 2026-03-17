10f6a793b45887a7f437de25928ae73effea41a8
Yuvasree-M <yuvasreemohan4sep2003@gmail.com>
2026-03-17 11:59:11 +0530

    Category Updated and Optimized

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,8 +1,8 @@
 import { readFileSync } from "fs";
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(readFileSync("./serviceAccountKey.json", "utf8"));
-// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// const serviceAccount = JSON.parse(readFileSync("./serviceAccountKey.json", "utf8"));
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
